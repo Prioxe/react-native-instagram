a16af9a3fd8ccb05ec2ce5e370178b81dd504269
Goku <jose_netopio_12@hotmail.com>
2019-07-16 03:02:32 -0300

    110. Tela pefil Usuário
    Foi criada a tela de perfil com seus estilos.

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
+import Profile from './screens/Profile'
 
 const MenuRoutes = {
     Feed: {
@@ -27,7 +28,7 @@ const MenuRoutes = {
     },
      Profile: {
          name: 'Profile',
-         screen: Feed,
+         screen: Profile,
          navigationOptions: {
              tittle: 'Profile',
              tabBarIcon: ({ tintColor }) =>
