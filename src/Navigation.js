a42810d28115b5b9548ca209a2a27e5b6e404da4
Goku <jose_netopio_12@hotmail.com>
2019-07-16 23:12:27 -0300

    111 Tela login e 112 Tela de Registro
    Estava com sono :(
    
    Foi criada a tela de login e depois de registro.
    
    Até o momento estou sem backend.

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,40 @@
 import React from 'react'
-import { createBottomTabNavigator } from 'react-navigation'
+import { 
+        createBottomTabNavigator,
+        createSwitchNavigator, 
+        createStackNavigator
+       } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
+import Login from './screens/Login'
+import Register from './screens/Register'
+
+
+const authRouter = createStackNavigator({
+
+    Login: { screen: Login, navigationOptions: { title: 'Login'}},
+    Register: { screen: Register, navigationOptions: { title: 'Register' }}
+ }  ,{
+        initialRouteName: 'Login'
+})
+
+const loginOrProfileRouter = createSwitchNavigator({
+    Profile: Profile,
+    Auth: authRouter
+}, {
+    initialRouteName: 'Profile'   
+})
+
 
 const MenuRoutes = {
     Feed: {
         name: 'Feed',
         screen: Feed,
         navigationOptions: {
-            tittle: 'Feed',
+            title: 'Feed',
             tabBarIcon: ({ tintColor }) =>
                 <Icon name='home' size={30} color={tintColor} />
         }
@@ -21,16 +44,16 @@ const MenuRoutes = {
         name: 'AddPhoto',
         screen: AddPhoto,
         navigationOptions: {
-            tittle: 'Add Picture',
+            title: 'Add Picture',
             tabBarIcon: ({ tintColor }) =>
                 <Icon name='camera' size={30} color={tintColor} />
         }
     },
      Profile: {
          name: 'Profile',
-         screen: Profile,
+         screen: loginOrProfileRouter,
          navigationOptions: {
-             tittle: 'Profile',
+             title: 'Profile',
              tabBarIcon: ({ tintColor }) =>
                 <Icon name='user' size={30} color={tintColor} />
          }
