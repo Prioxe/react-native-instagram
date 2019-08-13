e6e0cf4cec4a37b0a6c2a74c948733509a13090d
Goku <jose_netopio_12@hotmail.com>
2019-08-13 03:28:44 -0300

    128. Usando FIrebase Storage #2
    Mais configurações do firebase.

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true }) 
+//Liberando para varios dispositivos.
+const fs = require('fs') //Trabalhando com arquivos node mesmo.
+const uuid = require('uuid-v4') // indenticiador unico.
+const { Storage } = require('google-cloud/storage')
+const storage = new Storage({
+    projectId: 'lambe-react-native-code',
+    keyFilename: 'lambe-react-native-code-firebase.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.uploadImage = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
