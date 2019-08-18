ab185179cca9d27433ef6aa542c7251aa716b811
Goku <jose_netopio_12@hotmail.com>
2019-08-18 03:04:13 -0300

    129. Usando Firebase Storage #3
    Mais configurações do firebase.
    
    Jogando a imagem para o Storage do firebase.
    
    Fiz o deploy no firebase.

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')({ origin: true })
 //Liberando para varios dispositivos.
 const fs = require('fs') //Trabalhando com arquivos node mesmo.
 const uuid = require('uuid-v4') // indenticiador unico.
-const { Storage } = require('google-cloud/storage')
+const { Storage } = require('@google-cloud/storage')
 const storage = new Storage({
     projectId: 'lambe-react-native-code',
     keyFilename: 'lambe-react-native-code-firebase.json'
@@ -12,5 +12,37 @@ const storage = new Storage({
 
 
 exports.uploadImage = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ cors(request, response, () => {
+     try{
+         fs.writeFileSync('/tmp/imageToSave.jpg',
+             request.body.image, 'base64')
+
+        const bucket = storage.bucket('lambe-react-native-code.appspot.com')
+        const id = uuid()
+        bucket.upload('/tmp/imageTosave.jpg', {
+            uploadType: 'media',
+            destination: `/post/${id}.jpg`,
+            metadata:{
+                metadata: {
+                    contentType: 'image/jpeg',
+                    firebaseStorageDownloadTokens: id
+                }
+            }
+        }, (err,file) =>{
+            if (err) {
+                console.log(err)
+                return response.status(500).json({ error: err})
+                }  else {
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b'
+                        + bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+                    return response.status(201).json({ imageUrl : imageUrl })    
+                }
+         })
+     } catch (err) {
+         console.log(err)
+         return response.status(500).json({ error: err})
+     }
+     
+    })
 });
